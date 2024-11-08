351efb6ba3f22e47183d2c5428ec004da6cce5f8
maximadoma <fernylmaningo@gmail.com>
2024-11-08 13:18:53 +0800

    final upload -templates for user login/sign-up

---
+++ apps/users/templates/function.js
@@ -0,0 +1,18 @@
+const html = document.getElementById("htmlPage");
+const check = document.getElementById("switch-check");
+const label = document.querySelector('label[for="switch-check"]');
+
+function updateLabel() {
+    if (!check.checked) {
+        html.setAttribute("data-bs-theme", "light");
+        label.textContent = "Light mode";
+    } else {
+        html.setAttribute("data-bs-theme", "dark");
+        label.textContent = "Dark mode"; 
+    }
+}
+
+updateLabel();
+
+
+check.addEventListener("change", updateLabel);
