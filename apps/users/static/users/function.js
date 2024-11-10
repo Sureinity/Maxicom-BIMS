4105cf425947cd7076ae302bca1b5fe487efbc49
maximadoma <fernylmaningo@gmail.com>
2024-11-10 14:24:28 +0800

    Added 2 files: barcode input & book details | Modified the CSS file

--- apps/users/static/users/function.js
+++ apps/users/static/users/function.js
@@ -3,16 +3,15 @@ const check = document.getElementById("switch-check");
 const label = document.querySelector('label[for="switch-check"]');
 
 function updateLabel() {
-    if (!check.checked) {
-        html.setAttribute("data-bs-theme", "light");
-        label.textContent = "Light mode";
-    } else {
-        html.setAttribute("data-bs-theme", "dark");
-        label.textContent = "Dark mode"; 
-    }
+  if (!check.checked) {
+    html.setAttribute("data-bs-theme", "light");
+    label.textContent = "Light mode";
+  } else {
+    html.setAttribute("data-bs-theme", "dark");
+    label.textContent = "Dark mode";
+  }
 }
 
 updateLabel();
-
 
 check.addEventListener("change", updateLabel);
