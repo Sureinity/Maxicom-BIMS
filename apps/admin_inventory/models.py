6f2b6eee9959249c2d32e5aa1339be69f60b0c37
Sureinity <johnghlendealdo@gmail.com>
2024-11-28 22:27:09 +0800

    Working on models: book_inventory & book_list | New field added for 'users' model: sys_status

--- apps/admin_inventory/models.py
+++ apps/admin_inventory/models.py
@@ -2,24 +2,44 @@ from django.db import models
 
 # Create your models here.
 
-    #Partial model layout for Booklist
-# class Booklist(models.Model):
-#     id = models.BigAutoField(primary_key=True, db_column="b_id")
-#     item_call_number = models.CharField(max_length="11", blank=True, db_column="b_col_code")
-#     barcode = models.IntegerField(db_column="b_barcode")
-#     col_code = models.CharField(max_length="11", blank=True, db_column="b_col_code")
-#     title = models.CharField(max_length=255, blank=True, db_column="b_title")
-#     author = models.CharField(max_length=255, blank=True, db_column="b_author")
-#     publisher = models.CharField(max_length=255, blank=True, db_column="b_publisher")
-#     date_pub = models.DateField(auto_now=True, db_column="b_date_pub")
-#     place_pub = models.CharField(max_length=255, db_column="b_place_pub")
-#     isbn = models.IntegerField(db_column="b_isbn")
-#     date_acquired = models.DateField(auto_now=True, db_column="b_date_acquired")
-#     copy_no = models.IntegerField(db_column="b_copy_no")
-#     volume_no = models.IntegerField(db_column="b_volume_no")
-#     edition_stmt = models.CharField(max_length=255, db_column="b_edition_stmt")
-#     price = models.DecimalField(max_digits=10, decimal_places=2, db_column="b_price")
-#
-#     #Partial model layout for Inventory
-# #    class Inventory(models.Model):
+#Partial model layout for Booklist
+class Booklist(models.Model):
+    id = models.BigAutoField(primary_key=True, db_column="b_id")
+    item_call_number = models.CharField(max_length="11", blank=True, db_column="b_col_code")
+    barcode = models.IntegerField(db_column="b_barcode")
+    col_code = models.CharField(max_length="11", blank=True, db_column="b_col_code")
+    title = models.CharField(max_length=255, blank=True, db_column="b_title")
+    author = models.CharField(max_length=255, blank=True, db_column="b_author")
+    publisher = models.CharField(max_length=255, blank=True, db_column="b_publisher")
+    date_pub = models.DateField(auto_now=True, db_column="b_date_pub")
+    place_pub = models.CharField(max_length=255, db_column="b_place_pub")
+    isbn = models.IntegerField(db_column="b_isbn")
+    date_acquired = models.DateField(auto_now=True, db_column="b_date_acquired")
+    copy_no = models.IntegerField(db_column="b_copy_no")
+    volume_no = models.IntegerField(db_column="b_volume_no")
+    edition_stmt = models.CharField(max_length=255, db_column="b_edition_stmt")
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_column="b_price")
+
+    #Partial model layout for Inventory
+class Inventory(models.Model):
+    # Book status
+    GOOD_CONDITION = 1
+    NO_BARCODE_TAG = 2
+    FOR_REPAIR = 3
+    FOR_DISPOSAL = 4
+    
+    BOOK_STATUS_CHOICES = [
+        (GOOD_CONDITION, 'Good Condition'),
+        (NO_BARCODE_TAG, 'No Barcode Tag'),
+        (FOR_REPAIR, 'For Repair'),
+        (FOR_DISPOSAL, 'For Disposal'),
+    ]
+
+    id = models.BigAutoField(primary_key=True, db_column="inv_id")
+    b_id = models.ForeignKey(Booklist, on_delete=models.CASCADE)
+    datetime_checked = models.DatetimeField(auto_now=True, db_column="inv_datetime_checked")
+    status =  models.IntegerField(choices=BOOK_STATUS_CHOICES, null=True)
+
+
+
 
--- apps/users/models.py
+++ apps/users/models.py
@@ -2,18 +2,29 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    first_name = None
-    last_name = None
-    email = None
-    date_joined = None
-
+    # User role
     ADMIN = 0
     USER = 1
-    
     ROLE_CHOICES = [
         (ADMIN, 'Admin'),
         (USER, 'User'),
     ]
+
+    # User account status
+    ACTIVE = 0
+    SUSPENDED = 1
+    DELETED = 2 # Soft delete
+    STATUS_CHOICES = [
+        (ACTIVE, 'Active'),
+        (SUSPENDED, 'Suspended'),
+        (DELETED, 'Deleted'),
+
+    ]
+
+    first_name = None
+    last_name = None
+    email = None
+    date_joined = None
 
     id = models.AutoField(primary_key=True, db_column='sys_acc_id')
     username = models.CharField(
@@ -27,10 +38,9 @@ class User(AbstractUser):
     password = models.CharField(max_length=128, db_column='sys_password')
     sys_firstname = models.CharField(max_length=150, blank=True)
     sys_lastname = models.CharField(max_length=150, blank=True)
-    sys_acc_role = models.IntegerField(
-        choices=ROLE_CHOICES,
-        default=USER,
-    )
+    sys_acc_created = models.DateTimeField(auto_now_add=True) # Automatically set when user is created
+    sys_acc_role = models.IntegerField(choices=ROLE_CHOICES, default=USER)
+    sys_status = models.IntegerField(choices=STATUS_CHOICE, default=ACTIVE)
 
     REQUIRED_FIELDS = ['sys_acc_role']
     USERNAME_FIELD = 'username'
