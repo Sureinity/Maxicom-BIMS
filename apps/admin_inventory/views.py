e74d2f6ce6ade012ffbbc3b13b0ea0d7942b4d20
Sureinity <johnghlendealdo@gmail.com>
2024-11-30 09:57:22 +0800

    smol change on isbn field

--- apps/admin_inventory/views.py
+++ apps/admin_inventory/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
 
 from .decorators import admin_required
 
@@ -19,14 +18,7 @@ def dashboard_page(request):
 
 @admin_required
 def listbooks_page(request):
-    data = range(1, 101)  # Example data: numbers from 1 to 100
-    paginator = Paginator(data, 10)  # 10 items per page
-
-    # Get the current page number from the request, default to the first page
-    page_number = request.GET.get('page', 1)
-    page_obj = paginator.get_page(page_number)  # Automatically handles invalid page numbers
-
-    return render(request, "pages/listbooks_page.html", {'page_obj': page_obj})
+    return render (request, "pages/listbooks_page.html")
 
 @admin_required
 def inventory_page(request):
