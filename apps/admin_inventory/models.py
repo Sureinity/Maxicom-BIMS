ca9c23dc373b7dbcfcf4a7f4817b796d3716f8ec
Sureinity <johnghlendealdo@gmail.com>
2024-12-08 22:11:23 +0800

    admin_inventory: Changed the all CharField fields' max length to 255

--- apps/admin_inventory/models.py
+++ apps/admin_inventory/models.py
@@ -9,23 +9,23 @@ from apps.users.models import User
 
 class Booklist(models.Model):
     id = models.BigAutoField(primary_key=True, db_column="b_id")
-    item_call_num = models.CharField(max_length=200, blank=False, db_column="b_item_call_num")
-    col_code = models.CharField(max_length=100, blank=False, db_column="b_col_code")
-    barcode = models.CharField(max_length=55, blank=False, db_column="b_barcode")
-    itype = models.CharField(max_length=100, blank=False, db_column="b_itype")
+    item_call_num = models.CharField(max_length=255, blank=False, db_column="b_item_call_num")
+    col_code = models.CharField(max_length=255, blank=False, db_column="b_col_code")
+    barcode = models.CharField(max_length=255, blank=False, db_column="b_barcode")
+    itype = models.CharField(max_length=255, blank=False, db_column="b_itype")
     title = models.CharField(max_length=255, blank=False, db_column="b_title")
     author = models.CharField(max_length=255, blank=False, db_column="b_author")
     publisher_code = models.CharField(max_length=255, blank=True, db_column="b_publisher_code")
-    date_accessioned = models.CharField(max_length=55, blank=True, db_column="b_date_accessioned")
-    copyrightdate = models.CharField(max_length=55, blank=True, db_column="b_copyrightdate")
-    isbn = models.CharField(max_length=20, blank=False, db_column="b_isbn")
-    copy_num = models.CharField(max_length=55, db_column="b_copy_num", blank=True,  help_text="Copy number starting from 1")
-    volume = models.CharField(max_length=100, blank=True, null=True, db_column="b_volume")
+    date_accessioned = models.CharField(max_length=255, blank=True, db_column="b_date_accessioned")
+    copyrightdate = models.CharField(max_length=255, blank=True, db_column="b_copyrightdate")
+    isbn = models.CharField(max_length=255, blank=False, db_column="b_isbn")
+    copy_num = models.CharField(max_length=255, db_column="b_copy_num", blank=True,  help_text="Copy number starting from 1")
+    volume = models.CharField(max_length=255, blank=True, null=True, db_column="b_volume")
     edition_stmt = models.CharField(max_length=255, blank=True, null=True, db_column="b_edition_stmt")
     subtitle = models.TextField(db_column="b_subtitle", blank=True, null=True)
-    paidfor = models.CharField(max_length=55, blank=True, null=True, db_column="b_paidfor")
-    price = models.CharField(max_length=55, null=True, blank=True, db_column="b_price") # PHP Peso
-    bookseller_id = models.CharField(max_length=55, blank=False, db_column="b_bookseller_id")
+    paidfor = models.CharField(max_length=255, blank=True, null=True, db_column="b_paidfor")
+    price = models.CharField(max_length=255, null=True, blank=True, db_column="b_price") # PHP Peso
+    bookseller_id = models.CharField(max_length=255, blank=False, db_column="b_bookseller_id")
 
     class Meta:
         ordering = ['title']
